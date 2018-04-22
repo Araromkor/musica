8b14562017a2751c81c22a8c9f3286a462d9e10e
Tagir Faizullin <demoflava@gmail.com>
2018-04-22 15:16:42 +0500

    created an example

---
+++ Baraban_4world/Assets/Button1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Button1 : MonoBehaviour, IVirtualButtonEventHandler {
+	public AudioClip Clip;
+	public AudioSource ASource;
+	public GameObject Btn1;
+	public GameObject Btn2;
+
+	// Use this for initialization
+	void Start () {
+		Btn1 = GameObject.Find("VButton1");
+		Btn2 = GameObject.Find("VButton2");
+		Btn1.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		Btn2.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		ASource = GetComponent<AudioSource> ();
+	}
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+		ASource.clip = Clip;
+		ASource.Play ();
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb)
+	{
+
+	}
+
+}﻿
---
+++ Baraban_4world/Assets/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonScript : MonoBehaviour, IVirtualButtonEventHandler {
+	
+	public GameObject Btn1;
+	public GameObject Btn2;
+
+	// Use this for initialization
+	void Start () {
+		Btn1 = GameObject.Find("VButton1");
+		Btn2 = GameObject.Find("VButton2");
+		Btn1.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+		Btn2.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+	}
+	public void OnButtonPressed(VirtualButtonBehaviour vb)
+	{
+		
+	}
+
+	public void OnButtonReleased(VirtualButtonBehaviour vb)
+	{
+
+	}
+
+}﻿
